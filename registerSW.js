9bafbff35ef87171d901950b7a280b474ac25335
nicunursekatie <katielong2316@gmail.com>
2025-05-13 02:10:43 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ADHDplanner/sw.js', { scope: '/ADHDplanner/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ADHDPlannerWorking/sw.js', { scope: '/ADHDPlannerWorking/' })})}
